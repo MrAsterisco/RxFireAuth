cb5d403cb42b3e34bc8ade6240898faba6e6ba3c
Alessio Moiso <a.moiso@outlook.com>
2020-04-25 21:54:28 +0200

    Add documentation on LoginCredentials

--- RxFireAuth/Classes/LoginCredentials.swift
+++ RxFireAuth/Classes/LoginCredentials.swift
@@ -7,20 +7,34 @@
 
 import Foundation
 
+/// An instance of this class is returned by
+/// methods of `LoginProviderManagerType`
+/// when a `UserError.migrationRequired` error occurs.
+///
+/// You shouldn't need to inspect the content of this struct.
+/// Its main purpose is to temporary store credentials in order
+/// to continue the login action when clients have handled the error.
 public struct LoginCredentials {
     
+    /// A provider represent a supported login provider.
     public enum Provider: String {
+        /// Sign in with Apple.
         case apple = "apple.com"
     }
     
+    /// Get or set the ID token.
     var idToken: String
     
+    /// Get or set the user full name.
     var fullName: String?
     
+    /// Get or set the user email.
     var email: String
     
+    /// Get or set the login provider.
     var provider: Provider
     
+    /// Get or set the nonce.
     var nonce: String
     
 }
