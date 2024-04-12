f999e6710194d1d16948d287093898e10e4146d4
Alessio Moiso <alessio@inerziasoft.eu>
2024-04-12 10:56:18 +0200

    Remove explicit dependency to AppAuth

--- Package.swift
+++ Package.swift
@@ -18,7 +18,6 @@ let package = Package(
     .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", from: "10.0.0"),
     .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", from: "2.0.0"),
     .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift", from: "6.0.0"),
-    .package(name: "AppAuth", url: "https://github.com/openid/AppAuth-iOS", from: "1.0.0"),
 		.package(name: "GoogleSignIn", url: "https://github.com/google/GoogleSignIn-iOS", from: "7.0.0"),
 		.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
   ],
@@ -30,7 +29,6 @@ let package = Package(
         .product(name: "JWTDecode", package: "JWTDecode"),
         .product(name: "RxSwift", package: "RxSwift"),
         .product(name: "RxCocoa", package: "RxSwift"),
-        .product(name: "AppAuth", package: "AppAuth"),
 				.product(name: "GoogleSignIn", package: "GoogleSignIn")
       ],
       path: "RxFireAuth",
